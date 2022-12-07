cf3e42948faf4a71993fd0d6d80bf2b2a5eaa7e2
Joshua Leong <90120658+JoshuaLSE@users.noreply.github.com>
2022-12-08 01:31:32 +0800

    Update new_task.py

--- new_task.py
+++ new_task.py
@@ -1,20 +1,78 @@
-#!/usr/bin/env python
+"""
+Test Scripts
+User will run the script in python to load the rabbitMQ queues
+
+The database shall preload with a set of 1000 items.
+
+1) Which Queue do they want to load?
+2) How many queue items do they want to load?
+3) What are the sort of queue items they want to load?
+"""
+import random
 import pika
-import sys
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
+"""
+Get User Input
+"""
 
-channel.queue_declare(queue='task_queue', durable=True)
+queueTxt = "Which queue? "
+itemsTxt = "How many queue items? "
+randRangeTxt = "Range for randomisation? "
 
-message = ' '.join(sys.argv[1:]) or "Hello World!"
-channel.basic_publish(
-    exchange='',  # This publishes the thing to a default exchange
-    routing_key='task_queue',
-    body=message,
-    properties=pika.BasicProperties(
-        delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
-    ))
-print(" [x] Sent %r" % message)
-connection.close()
+queue = input(queueTxt)
+items = input(itemsTxt)
+randRange = input(randRangeTxt)
+
+if queue.isdigit() or items.isalpha() or randRange.isalpha():
+    print("Queue must be alphabetical, Items & Random Range must be digit")
+    exit(1)
+
+items = int(items)  # This might be read from a file instead
+randRange = int(randRange)
+
+"""
+Connecting to RabbitMQ
+"""
+
+try:
+    # Queue Declaration
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+
+    def publish_message(queueName, message):
+        """
+        Publishes message to destined queueName with message
+        """
+        channel.basic_publish(
+            exchange='',  # This publishes the thing to a default exchange
+            routing_key=queueName,
+            body=message,
+            properties=pika.BasicProperties(
+                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+            ))
+        print(" [x] Sent %r" % message)
+        # connection.close()
+
+except Exception as e:
+    txt = "\nYour RabbitMQ server might not be running"
+    print(txt.upper())
+    exit(2)
+
+"""
+Sending Messages
+"""
+
+for i in range(items + 1):
+    randomNumber = random.randrange(randRange)
+    publish_message(queue, str(randomNumber))
+
+"""
+Finish Sending
+"""
+
+finish = "\nTesting completed for:\nQueue: {}" \
+         "\nNumber of Items: {} " \
+         "\nRange for Item Randomisation: {}"
+
+print(finish.format(queue, items, randRange))
